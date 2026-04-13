a969cf81fce5517202c19fc5abb733f8d64193d9
shreya535 <165040844+shreya535@users.noreply.github.com>
2026-04-13 14:06:46 +0530

    Time: 32 ms (85.30%) | Memory: 41 MB (42.27%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -3,44 +3,23 @@ public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
         
-        // Step 1: prefix sum
-        vector<int> prefix(n + 1, 0);
+       
+        vector<int> arr(n);
         for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + (nums[i] == target ? 1 : -1);
+            arr[i] = (nums[i] == target) ? 1 : -1;
         }
 
-        // Step 2: coordinate compression
-        vector<int> all = prefix;
-        sort(all.begin(), all.end());
-        all.erase(unique(all.begin(), all.end()), all.end());
+        int count = 0;
 
-        // Fenwick Tree
-        vector<int> BIT(all.size() + 1, 0);
-
-        auto update = [&](int i) {
-            for (; i < BIT.size(); i += i & -i)
-                BIT[i]++;
-        };
-
-        auto query = [&](int i) {
-            int s = 0;
-            for (; i > 0; i -= i & -i)
-                s += BIT[i];
-            return s;
-        };
-
-        int ans = 0;
-
-        for (int i = 0; i <= n; i++) {
-            int idx = lower_bound(all.begin(), all.end(), prefix[i]) - all.begin() + 1;
-            
-            // count how many prefix values < current
-            ans += query(idx - 1);
-
-            // add current prefix
-            update(idx);
+        // Step 2: check all subarrays
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum > 0) count++;
+            }
         }
 
-        return ans;
+        return count;
     }
 };
